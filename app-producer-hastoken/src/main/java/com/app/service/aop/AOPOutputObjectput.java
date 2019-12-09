0d4d4a57b052ae15a47acfe7d43e7a1442660403
weizhiqiang <598748873@qq.com>
2019-12-09 14:28:29 +0800

    token获取方式修改

--- app-producer-hastoken/src/main/java/com/app/service/aop/AOPOutputObjectput.java
+++ app-producer-hastoken/src/main/java/com/app/service/aop/AOPOutputObjectput.java
@@ -1,5 +1,6 @@
 package com.app.service.aop;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -53,7 +54,7 @@ public class AOPOutputObjectput {
 		try {
 			logger.info("环绕通知开始");
 			//分别送请求和cookies中获取usertoken
-			String userToken = request.getParameter("userToken");
+			String userToken = getUserToken(request);
 			
 			//usertoken最终判断
 			if(ToolUtil.isBlank(userToken)) {
@@ -91,4 +92,41 @@ public class AOPOutputObjectput {
 	@AfterThrowing(value="execution(* com.app.producer.*.*(..))", throwing="e")
 	public void afterThrowing(JoinPoint joinPoint, Exception e) {
 	}
+	
+	/**
+	 * 获取userToken
+	 * @param request
+	 * @return
+	 */
+	public String getUserToken(HttpServletRequest request){
+		String userToken = request.getParameter("userToken");
+		//从cookies中获取
+		if(ToolUtil.isBlank(userToken)){
+			userToken = getCookiesByName(request, "userToken");
+		}
+		//从header中获取
+		if(ToolUtil.isBlank(userToken)){
+			userToken = request.getHeader("userToken");
+		}
+		return userToken;
+	}
+	
+	/**
+	 * 从cookies获取内容
+	 * @param request
+	 * @param name
+	 * @return
+	 */
+	public String getCookiesByName(HttpServletRequest request, String name){
+		Cookie[] cookies =  request.getCookies();
+		if(cookies != null){
+			for(Cookie cookie : cookies){
+				if(cookie.getName().equals(name)){
+					return cookie.getValue();
+				}
+			}
+		}
+		return "";
+	}
+	
 }
